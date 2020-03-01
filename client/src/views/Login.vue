6c9bb525331cc373a5a4b84c9fcd9a377d3fbda0
calvin <puram.calvin@gmail.com>
2020-03-01 11:58:27 +0100

    basic vue client clean up

---
+++ client/src/views/Login.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1 class="text-center mt-3">Login</h1>
+  </div>
+</template>
---
+++ client/src/views/Register.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1 class="text-center mt-3">Register</h1>
+  </div>
+</template>
