b1a020ce40c061379cac857deded4677b15101bb
calvin <puram.calvin@gmail.com>
2020-04-20 22:34:48 +0100

    completed testing the login auth

--- __test__/integration/auth/login.spec.js
+++ __test__/integration/auth/login.spec.js
@@ -22,11 +22,10 @@ describe('The Login Process', () => {
     passwordConfirm: '2begood4'
   };
 
-  let currentUser;
   beforeAll(async () => {
     await connectDB();
     await Users.deleteMany();
-    currentUser = await Users.create(user);
+    await Users.create(user);
   });
 
   it('should log user', async () => {
@@ -43,6 +42,48 @@ describe('The Login Process', () => {
     expect(res.body.success).toBe(true);
     expect(res.body.token).toBeDefined();
     expect(res.body.user).toBeDefined();
+  });
+
+  it('should throw if email and password are not provided', async () => {
+    const loggedInUser = {};
+
+    const res = await app()
+      .post(LOGIN_URL)
+      .send(loggedInUser);
+
+    expect(res.status).toBe(400);
+    expect(res.body.success).toBe(false);
+    expect(res.body.error).toBe('email and password are required');
+  });
+
+  it('show throw an error if no user is found', async () => {
+    const loggedInUser = {
+      email: 'xpuram@gmail.com',
+      password: '2begood4'
+    };
+
+    const res = await app()
+      .post(LOGIN_URL)
+      .send(loggedInUser);
+
+    expect(res.status).toBe(401);
+    expect(res.body.success).toBe(false);
+    expect(res.body.error).toBe('Invalid Credentials');
+  });
+
+  it('show throw an error if no user password is invalid', async () => {
+    const loggedInUser = {
+      email: user.email,
+      password: '2begood444'
+    };
+
+    const res = await app()
+      .post(LOGIN_URL)
+      .send(loggedInUser);
+
+    expect(res.status).toBe(401);
+    expect(res.body.success).toBe(false);
+    expect(res.body.error).toBe('Invalid Credentials');
   });
 
   afterAll(async () => {
