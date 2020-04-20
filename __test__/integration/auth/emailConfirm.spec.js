c52d55a214f098cef73df764fff04170c4f27803
calvin <puram.calvin@gmail.com>
2020-04-20 18:42:19 +0100

    finish integration process for confirm account

---
+++ __test__/integration/auth/emailConfirm.spec.js
@@ -0,0 +1,61 @@
+/**
+ * @jest-environment node
+ */
+
+/* eslint-disable no-undef */
+
+const supertest = require('supertest');
+
+const { connectDB, closeDB } = require('../../server/utils/mongoose');
+const Users = require('../../../models/Users');
+const server = require('../../../app');
+const auth = require('../../../controller/authController');
+const AppError = require('../../../utils/custormError');
+const Response = require('../../server/utils/response');
+
+const app = () => supertest(server);
+const confirmEmailURL = '/api/v1/auth/email/confirm';
+
+describe('The Email Confirm Process', () => {
+  const user = {
+    name: 'puram calvin',
+    email: 'cpuram@gmail.com',
+    password: '2begood4',
+    passwordConfirm: '2begood4'
+  };
+
+  let currentUser;
+  beforeAll(async () => {
+    await connectDB();
+    await Users.deleteMany();
+    currentUser = await Users.create(user);
+  });
+
+  it('should throw error if user is not found with the token provided', async () => {
+    const req = {
+      body: { token: 'asasa' }
+    };
+    const res = new Response();
+    const next = jest.fn();
+
+    await auth.confirmAccount(req, res, next);
+
+    expect(next).toHaveBeenCalledWith(new AppError('invalid credentials', 401));
+  });
+
+  it('should set confirm email to users account', async () => {
+    const token = await currentUser.emailConfirmCode;
+
+    const res = await app()
+      .patch(confirmEmailURL)
+      .send({ token });
+
+    expect(res.status).toBe(200);
+    expect(res.body.success).toBe(true);
+    expect(res.body.user).toBeDefined();
+  });
+
+  afterAll(async () => {
+    await closeDB();
+  });
+});
