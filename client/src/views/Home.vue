d12310f424366957b13044ed80914e3726ea5248
calvin <puram.calvin@gmail.com>
2020-04-22 19:33:54 +0100

    test the home page title

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="d-flex justify-content-center align-items-center mt-5">
     <h1 v-if="getUserName">Welcome {{ getUserName.name }}</h1>
-    <h1 class="d-flex justify-content-center align-items-center mt-5" v-else>
+    <h1
+      id="home-page-title"
+      class="d-flex justify-content-center align-items-center mt-5"
+      v-else
+    >
       Fullstack Auth Application
     </h1>
   </div>
--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-undef */
 describe('The Home Page', () => {
-  it('should navigate to home', () => {
+  it('should navigate to home and display page title', () => {
     cy.visit('/');
+
+    cy.get('#home-page-title').should('contain', 'Fullstack Auth Application');
   });
 });
