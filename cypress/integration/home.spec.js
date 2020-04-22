32c7ddd9528a3590474cece831694ae06d7b43d0
calvin <puram.calvin@gmail.com>
2020-04-22 19:48:13 +0100

    test navigation to login page

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -12,4 +12,12 @@ describe('The Home Page', () => {
     cy.get('#register-button').should('contain', 'Register');
     cy.get('#login-button').should('contain', 'Login');
   });
+
+  it('should navigate to login page when clicked', () => {
+    cy.visit('/');
+
+    cy.get('#login-button').click();
+
+    cy.url().should('contain', '/login');
+  });
 });
